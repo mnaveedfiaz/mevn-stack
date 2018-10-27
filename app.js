fd7d4b164c9979f27a7c38af8c1d6aba70ab0128
Naveed Fiaz <naveed.codexbay@gmail.com>
2018-10-27 15:31:50 +0500

    connected to db

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+const db             = require('./config/db');
 
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, ()=>{
-	console.log("Server started at port:"+port);
+MongoClient.connect("mongodb://127.0.0.1:27017/contactsDb",{ useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err)
+  console.log("Connected to mongodb");	
+  require('./app/routes')(app, database);
+	
+	app.listen(port, ()=>{
+		console.log("Server started at port:"+port);
+	});
 });
+
--- app/routes/contact_routes.js
+++ app/routes/contact_routes.js
@@ -1,7 +1,13 @@
 module.exports = function(app, db) {
 	app.post('/contacts', (req, res) => {
-	  console.log(req.body)
-	  res.send('Hello')
+	  const note = { text: req.body.body, title: req.body.title };
+    db.collection('contacts').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
   app.get('/contacts', (req, res) => {
 	  res.send('Hello')
