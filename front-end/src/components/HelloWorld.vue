68d1f3113a2bb1b7ea1e9331426a221197c31c98
Naveed Fiaz <naveed.codexbay@gmail.com>
2018-11-16 00:02:21 +0500

    first chapter of vue js

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -1,113 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
+  <div v-bind:title="title" v-if="seen">
+    <h1>{{ message }}</h1>
+    <ol>
+      <li v-for="todo in todolist"
+      v-bind:key="todo.id">
+        {{todo.text}}
       </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+      <todo-item
+      v-for="item in groceryList"
+      v-bind:todo="item"
+      v-bind:key="item.id"></todo-item>
+    </ol>
+    <input v-model="message">
+    <button v-on:click="reverseMessage">Reverse Message</button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      message: 'Sample Vue js application',
+      title: 'You loaded this page on ' + new Date().toLocaleString(),
+      seen: true,
+      todolist: [
+        {id: 1, text: 'Urdu'},
+        {id: 2, text: 'English'},
+        {id: 3, text: 'Mathematics'}
+      ],
+      groceryList: [
+        { id: 4, text: 'Vegetables' },
+        { id: 5, text: 'Cheese' },
+        { id: 6, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
     }
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
